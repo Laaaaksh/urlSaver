fcc0594bda51bd783103036c67d406c1d8aaedce
Laksh Sadhwani <47391874+Laaaaksh@users.noreply.github.com>
2021-10-04 11:37:03 +0530

    added the list items functionality

--- jsfile.js
+++ jsfile.js
@@ -1,4 +1,19 @@
-let inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked from event listner")
+let myleads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function () {
+    myleads.push(inputEl.value)
+    RenderFunc()
 })
+function RenderFunc()
+{
+    let listItems = ""
+    for(let i = 0 ; i < myleads.length ; i++)
+    {
+        listItems +=  "<li>" + myleads[i]+ "</li>"
+        console.log(listItems)
+    }
+    ulEl.innerHTML = listItems
+}
